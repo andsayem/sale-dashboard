0a192aa4fb0274e4c4bafdbc81f11326964cfc24
AS Sayem <andsayem@gmail.com>
2022-02-24 12:39:38 +0600

    Auto Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,46 @@
 import 'package:flutter/material.dart';
 // import 'package:shopx/views/homepage.dart';
 import 'package:shopx/views/screens/login/login_screen.dart';
-import 'package:shopx/Login/loginScreen.dart';
-import 'constants.dart';
-import 'package:shopx/routes/routes.dart';
-GlobalKey globalKey = GlobalKey();
-void main() {
-  runApp(MyApp());
-}
+//import 'package:shopx/Login/loginScreen.dart';
+import 'package:shopx/views/screens/tabs/tabs.dart';
+//import 'constants.dart';
+//import 'package:shopx/routes/routes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+GlobalKey globalKey = GlobalKey(); 
 
-class MyApp extends StatelessWidget {
+void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  bool _isLoggedIn = false;
+
+  @override
+  void initState() {
+    _checkIfLoggedIn();
+    super.initState();
+  }
+  void _checkIfLoggedIn() async{
+      // check if token is there
+      SharedPreferences localStorage = await SharedPreferences.getInstance();
+      var token = localStorage.getString('token');
+      if(token!= null){
+         setState(() {
+            _isLoggedIn = true;
+         });
+      }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Opening View Demo',
-      routes: AppRoutes.define(),
-      key: globalKey,
-      home: LoginScreen(),
-     // home: LogIn(),
-    );
-    // return MaterialApp(
-    //   theme: ThemeData(
-    //      primarySwatch: CustomeTheme.primaryLight,
-    //     visualDensity: VisualDensity.adaptivePlatformDensity,
-    //   ),
-    //   debugShowCheckedModeBanner: false,
-    //   home:LoginScreen(),
-    // );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: _isLoggedIn ? Tabs() :  LoginScreen(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: Icon(Icons.add)),
+      
     );
   }
 }
