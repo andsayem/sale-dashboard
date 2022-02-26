22de78cd23ffc8ddd352983e7e1798e208740cb8
Smd Tanjib <smd.tanjib@gmail.com>
2022-02-26 11:21:04 +0600

    drawer (updated)

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,16 @@ import 'package:dashboard/views/screens/tabs/tabs.dart';
 //import 'constants.dart';
 //import 'package:dashboard/routes/routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-GlobalKey globalKey = GlobalKey(); 
+
+GlobalKey globalKey = GlobalKey();
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatefulWidget {
-  
   @override
   _MyAppState createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
   bool _isLoggedIn = false;
 
@@ -22,15 +24,16 @@ class _MyAppState extends State<MyApp> {
     _checkIfLoggedIn();
     super.initState();
   }
-  void _checkIfLoggedIn() async{
-      // check if token is there
-      SharedPreferences localStorage = await SharedPreferences.getInstance();
-      var token = localStorage.getString('token');
-      if(token!= null){
-         setState(() {
-            _isLoggedIn = true;
-         });
-      }
+
+  void _checkIfLoggedIn() async {
+    // check if token is there
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var token = localStorage.getString('token');
+    if (token != null) {
+      setState(() {
+        _isLoggedIn = true;
+      });
+    }
   }
 
   @override
@@ -38,9 +41,8 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: _isLoggedIn ? Tabs() :  LoginScreen(),
+        body: _isLoggedIn ? Tabs() : LoginScreen(),
       ),
-      
     );
   }
 }
